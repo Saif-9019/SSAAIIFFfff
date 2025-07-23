ae1185bffaa71942cd87037459cf4d10dce1974a
Saif-9019 <157525657+Saif-9019@users.noreply.github.com>
2025-07-23 17:41:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("donation-form").addEventListener("submit", function (e)
 
   const name = document.getElementById("name").value;
   const amount = document.getElementById("amount").value;
-  const upiLink = `upi://pay?pa=syedmohammedsaifenadeem-1@oksbi&pn=Darul Uloom Noorul Islam&am=${amount}&cu=INR`;
+  const upiLink = `upi://pay?pa=amjadsyed824@okhdfcbank=Darul Uloom Noorul Islam&am=${amount}&cu=INR`;
 
   alert("Thank you " + name + "! Redirecting to UPI...");
 
